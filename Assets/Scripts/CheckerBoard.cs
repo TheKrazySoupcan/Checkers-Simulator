bc8102de2e0dc725ce8173a8428e6576d4b7d429
thekrazysoupcan <thekrazysoupcan@gmail.com>
2017-05-18 15:32:32 +1000

    Bananakin

--- Assets/Scripts/CheckerBoard.cs
+++ Assets/Scripts/CheckerBoard.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CheckerBoard : MonoBehaviour
 {
+    public string fileName;
     public GameObject blackPiece;
     public GameObject whitePiece;
 
@@ -22,68 +23,141 @@ public class CheckerBoard : MonoBehaviour
         halfBoardX = boardX / 2;
         halfBoardZ = boardZ / 2;
         pieceDiameter = pieceRadius * 2f;
-        bottomLeft = transform.position - Vector3.right * halfBoardX - Vector3.forward * halfBoardZ;
-
+        bottomLeft = transform.position - Vector3.right * halfBoardX
+                                        - Vector3.forward * halfBoardZ;
         CreateGrid();
-
     }
 
     void CreateGrid()
     {
+        // Initialize the 2D array
         pieces = new Piece[boardX, boardZ];
 
         #region Generate White Pieces
-        for(int x = 0; x < boardX; x+=2)
+        // Loop through board columns and skip 2 each time
+        for (int x = 0; x < boardX; x += 2)
         {
-            for(int z = 0; z < 3; z++)
+            // Loop through first 3 rows
+            for (int z = 0; z < 3; z++)
             {
                 bool evenRow = z % 2 == 0;
+                // Ternary Operator
                 int gridX = evenRow ? x : x + 1;
                 int gridZ = z;
+                // Generate the piece
                 GeneratePiece(whitePiece, gridX, gridZ);
             }
         }
-
         #endregion
+
         #region Generate Black Pieces
-        for(int x = 0; x < boardZ; x+=2)
+        // Loop through board columns and skip 2 each time
+        for (int x = 0; x < boardX; x += 2)
         {
-            for(int z = 0; z > 3; z--)
+            // Loop through last 3 rows
+            for (int z = boardZ - 3; z < boardZ; z++)
             {
-                bool evenRow = x % 2 == 0;
+                bool evenRow = z % 2 == 0;
+                // Ternary Operator
                 int gridX = evenRow ? x : x + 1;
                 int gridZ = z;
+                // Generate the piece
                 GeneratePiece(blackPiece, gridX, gridZ);
-
             }
-
         }
         #endregion
     }
 
     void GeneratePiece(GameObject piecePrefab, int x, int z)
     {
+        // Create an instance of piece
         GameObject clone = Instantiate(piecePrefab);
-
+        // Set the parent to be this transform
         clone.transform.SetParent(transform);
-
+        // Get the piece component from the clone
         Piece piece = clone.GetComponent<Piece>();
+        // Place the piece
         PlacePiece(piece, x, z);
-
     }
 
     void PlacePiece(Piece piece, int x, int z)
     {
+        // Calculate offset for piece based on coordinate
         float xOffset = x * pieceDiameter + pieceRadius;
         float zOffset = z * pieceDiameter + pieceRadius;
+        // Set piece's new grid coordinate
         piece.gridX = x;
         piece.gridZ = z;
-        piece.transform.position = bottomLeft + Vector3.right * xOffset + Vector3.forward * zOffset;
-
+        // Move piece physically to board coordinate
+        piece.transform.position = bottomLeft + Vector3.right * xOffset
+                                              + Vector3.forward * zOffset;
+        // Set piece in array slot
         pieces[x, z] = piece;
-         
     }
 
+    public void DropPiece(Piece piece, Vector3 position)
+    {
+        // Translate position to coordinate in array
+        float percentX = (position.x + halfBoardX) / boardX;
+        float percentZ = (position.z + halfBoardZ) / boardZ;
+
+        percentX = Mathf.Clamp01(percentX);
+        percentZ = Mathf.Clamp01(percentZ);
+
+        int x = Mathf.RoundToInt((boardX - 1) * percentX);
+        int z = Mathf.RoundToInt((boardZ - 1) * percentZ);
+
+        if (IsValid(x, z))
+        {
+
+            // Get old piece from that coordinate
+            Piece oldPiece = pieces[x, z];
+
+            if (oldPiece == null)
+            {
+                // Place Piece
+                int oldX = piece.gridX;
+                int oldZ = piece.gridZ;
+                pieces[oldX, oldZ] = null;
+                PlacePiece(piece, x, z);
+            }
+            else
+            {
+                SwapPieces(piece, oldPiece);
+            }
+        }
+        else
+        {
+            int gridX = piece.gridX;
+            int gridZ = piece.gridZ;
+            PlacePiece(piece, gridX, gridZ);  
+        }
+    }
+
+    bool IsValid(int x, int z)
+    {
+        bool evenRow = z % 2 == 0;
+        bool evenCol = x % 2 == 0;
+        bool oddRow = z % 2 == 1;
+        bool oddCol = x % 2 == 1;
+        return (evenRow && evenCol) || (oddRow && oddCol);
+    }
+
+    void SwapPieces(Piece a, Piece b)
+    {
+        if (a == null || b == null)
+            return;
+
+        int aX = a.gridX;
+        int aZ = a.gridZ;
+
+        int bX = b.gridX;
+        int bZ = b.gridZ;
+
+        PlacePiece(a, bX, bZ);
+        PlacePiece(b, aX, aZ);
+    }
+    
     // Update is called once per frame
     void Update()
     {
--- Assets/Scripts/PiecePicker.cs
+++ Assets/Scripts/PiecePicker.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 using GGL;
 
 public class PiecePicker : MonoBehaviour
@@ -13,53 +12,81 @@ public class PiecePicker : MonoBehaviour
 
     private Piece selectedPiece;
     private CheckerBoard board;
+    private Vector3 hitPoint;
+
     // Use this for initialization
     void Start()
     {
+        // Find the checkerboard in the scene
         board = FindObjectOfType<CheckerBoard>();
+        // Check errors
         if (board == null)
         {
-            Debug.LogError("there is no CheckerBoard in the Scene!");
-
+            Debug.LogError("There is no CheckerBoard in the scene!");
         }
     }
 
+    // Check if we are selecting a piece
     void CheckSelection()
     {
+        if (selectedPiece)
+        {
+            if (selectedPiece != null)
+                return;
+        }
+        // Creating a ray from camera mouse position to world
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         GizmosGL.color = Color.red;
-        GizmosGL.AddLine(ray.origin, ray.origin + ray.direction * rayDistance);
-        RaycastHit hit;
+        GizmosGL.AddLine(ray.origin, ray.origin + ray.direction * rayDistance, 0.1f, 0.1f);
+        // Check if the player hits the mouse button
         if(Input.GetMouseButtonDown(0))
         {
-            
-            if(Physics.Raycast(ray,out hit, rayDistance))
+            RaycastHit hit;
+            // Cast a ray to detect piece
+            if (Physics.Raycast(ray, out hit, rayDistance))
             {
+                // Set the selected piece to be the hit object
                 selectedPiece = hit.collider.GetComponent<Piece>();
-
+                // If the user did not hit a piece
                 if(selectedPiece == null)
                 {
-                    Debug.Log("Cannot pick up boject:" + hit.collider.name);
+                    Debug.Log("Cannot pick up object: " + hit.collider.name);
                 }
             }
         }
     }
+
+    // Move the selected piece if one is selected
     void MoveSelection()
     {
-        if(selectedPiece !=null)
+        // Check if a piece has been selected
+        if(selectedPiece != null)
         {
+            // Create a new ray from the camera
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             GizmosGL.color = Color.yellow;
-            GizmosGL.AddLine(ray.origin, ray.origin + ray.direction * rayDistance);
+            GizmosGL.AddLine(ray.origin, ray.origin + ray.direction * rayDistance, 0.1f, 0.1f);
             RaycastHit hit;
-
+            // Raycast to only hit objects that aren't pieces
             if(Physics.Raycast(ray, out hit, rayDistance, ~selectionIgnoreLayer))
             {
+                hitPoint = hit.point;
+                // Obtain the hit point
                 GizmosGL.color = Color.blue;
                 GizmosGL.AddSphere(hit.point, 0.5f);
-
+                // Move the piece to position
                 Vector3 piecePos = hit.point + Vector3.up * pieceHeight;
                 selectedPiece.transform.position = piecePos;
+            }
+
+            if(Input.GetMouseButtonUp(0))
+            {
+                // Drop the piece to hitpoint
+                Piece piece = selectedPiece.GetComponent<Piece>();
+                board.DropPiece(piece, hitPoint);
+
+                // Deselect the piece
+                selectedPiece = null;
             }
         }
     }
@@ -68,5 +95,6 @@ public class PiecePicker : MonoBehaviour
     void Update()
     {
         CheckSelection();
+        MoveSelection();
     }
 }
